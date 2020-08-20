d55ab63f2f1fb65a9524d8bc670bb6dc0bffcec7
mileverse <dev@mileverse.com>
2020-08-20 14:58:45 +0900

    하단 백서 링크 추가

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
     $('.app-store').click(function(){
         window.open("https://apps.apple.com/kr/app/%EB%A7%88%EC%9D%BC%EB%B2%8C%EC%8A%A4/id1521818622","_blank")
     })
-    $(".whitePaperBtn").click(function(){
+    $(".whitePaperBtn, .footer-btn").click(function(){
         $(this).data('lang') === "kr" ? window.open("https://drive.google.com/file/d/1xuifltQOWcW1P9CH2mIYq8PBXhc3ruxY/view?usp=sharing","_blank") : alert("업로드 예정입니다.")
     })
     $('#lang').change(function(e){

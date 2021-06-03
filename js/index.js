03f325f9d52f665404cf21b01a8f7f0c7eaf423b
jinkook <jinqook0126@naver.com>
2021-06-03 10:31:43 +0900

    redirect event page

--- js/index.js
+++ js/index.js
@@ -1,42 +1,43 @@
 $(document).ready(function(){
-    var agent = navigator.userAgent.toLowerCase();
+    location.href='https://mileverse.com/events'
+    // var agent = navigator.userAgent.toLowerCase();
 
-    if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
-        $(".language-btnWrap").css('right','0')
-    }
+    // if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
+    //     $(".language-btnWrap").css('right','0')
+    // }
     
-    $(window).scroll(function() {
-        $('.header').css({left: 0 - $(this).scrollLeft()});
-    });
+    // $(window).scroll(function() {
+    //     $('.header').css({left: 0 - $(this).scrollLeft()});
+    // });
     
-    $('.menu_link').click(function(){
-        var _target = $(this).data('target')
-        var position = {};
-        if (_target === "home") position.top = 0;
-        else if(_target === "business") position = $('.value-area').offset();
-        else if(_target === "app") position = $('.app-area').offset();
-        else if(_target === "token") position = $('.token-area').offset();
-        else if(_target === "roadmap") position = $('.road-area').offset();
-        else if(_target === "members") position = $('.team-area').offset();
-        else if(_target === "partners") position = $('.partners-area').offset();
-        else if(_target === "footer") position = $('footer').offset();
-        $("html,body").stop().animate({scrollTop:position.top},500)
-    })
+    // $('.menu_link').click(function(){
+    //     var _target = $(this).data('target')
+    //     var position = {};
+    //     if (_target === "home") position.top = 0;
+    //     else if(_target === "business") position = $('.value-area').offset();
+    //     else if(_target === "app") position = $('.app-area').offset();
+    //     else if(_target === "token") position = $('.token-area').offset();
+    //     else if(_target === "roadmap") position = $('.road-area').offset();
+    //     else if(_target === "members") position = $('.team-area').offset();
+    //     else if(_target === "partners") position = $('.partners-area').offset();
+    //     else if(_target === "footer") position = $('footer').offset();
+    //     $("html,body").stop().animate({scrollTop:position.top},500)
+    // })
 
-    $('.play-store').click(function(){
-        window.open("https://play.google.com/store/apps/details?id=com.cordova.mileverse","_blank")
-    })
-    $('.app-store').click(function(){
-        window.open("https://apps.apple.com/kr/app/%EB%A7%88%EC%9D%BC%EB%B2%8C%EC%8A%A4/id1521818622","_blank")
-    })
-    $(".whitePaperBtn, .footer-btn").click(function(){
-        if($(this).data('lang') === "kr") {
-            window.open("https://drive.google.com/file/d/1xuifltQOWcW1P9CH2mIYq8PBXhc3ruxY/view?usp=sharing","_blank")
-        } else {
-            window.open("https://drive.google.com/file/d/1lUH4fnsnpI8bCrgpFKa0mPPttV7yNHJn/view?usp=sharing","_blank")
-        }
-    })
-    $('#lang').change(function(e){
-        $(this).prop('checked') === true ? location.href= '/en' : location.href= '/'
-    })
+    // $('.play-store').click(function(){
+    //     window.open("https://play.google.com/store/apps/details?id=com.cordova.mileverse","_blank")
+    // })
+    // $('.app-store').click(function(){
+    //     window.open("https://apps.apple.com/kr/app/%EB%A7%88%EC%9D%BC%EB%B2%8C%EC%8A%A4/id1521818622","_blank")
+    // })
+    // $(".whitePaperBtn, .footer-btn").click(function(){
+    //     if($(this).data('lang') === "kr") {
+    //         window.open("https://drive.google.com/file/d/1xuifltQOWcW1P9CH2mIYq8PBXhc3ruxY/view?usp=sharing","_blank")
+    //     } else {
+    //         window.open("https://drive.google.com/file/d/1lUH4fnsnpI8bCrgpFKa0mPPttV7yNHJn/view?usp=sharing","_blank")
+    //     }
+    // })
+    // $('#lang').change(function(e){
+    //     $(this).prop('checked') === true ? location.href= '/en' : location.href= '/'
+    // })
 })

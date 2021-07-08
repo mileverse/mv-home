a3a47c5d2ad67f355416951f942434f7b5a5d8a6
jinkook <jinqook0126@naver.com>
2021-07-08 14:10:07 +0900

    renew /kr page

--- kr/js/index.js
+++ kr/js/index.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
     $('#headers').load('/kr/common/header.html');
     $('#footers').load('/kr/common/footer.html');
+
+    console.log($('.main').length)
+    if($('.main').length ===0) {
+        //scroll event;;
+        $(window).scroll(function(){
+            //스크롤 이동시 작동코드
+            if($(window).scrollTop() <=540) {
+                $('.header-wrap').removeClass("bl")
+            } else {
+                $('.header-wrap').addClass("bl")
+            }
+        });
+    } else {
+        $('.header-wrap').addClass("bl")
+    }
     $('.input-file').change(function(e){
         $('#fileTxt').text($(this).val());
     })
